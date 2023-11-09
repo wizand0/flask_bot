47d8c3185e7a055a804c6aa859e3ff675c530b9e
wizand0 <xabor@yandex.ru>
2023-11-09 15:10:26 +0300

    DB

--- config.py
+++ config.py
@@ -39,14 +39,14 @@ class DevelopementConfig(BaseConfig):
 
 class TestingConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///main.db'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('TESTING_DATABASE_URI') or \
 	#		      'mysql+pymysql://root:pass@localhost/flask_app_db'
 
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///main.db'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URI') or \
 	#'mysql+pymysql://root:pass@localhost/flask_app_db'
 
